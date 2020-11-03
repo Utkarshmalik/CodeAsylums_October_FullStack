f403c8cd448d710a46bfa5230454c0a9519067f7
mohitverma6161 <utkarshmalik06@gmail.com>
2020-11-04 00:55:49 +0530

    inheritence added everywhere

--- CourseProject/temp.js
+++ CourseProject/temp.js
@@ -1,92 +1,136 @@
-function course(name,author,date,ratings)
+
+class attribute
 {
+  constructor(name,value)
+  {
+    this.name=name;
+    this.value=value;
+  }
+}
+
+
+class Component
+{
+  createAndAppendElement(tag,className,attributes=[],id)
+  {
+    let newElement=document.createElement(tag);
+
+    if(className)
+    newElement.className=className;
+
+    console.log(attributes);
+
+    for(let i=0;i<attributes.length;i++)
+    {
+    
+      newElement[attributes[i].name]=attributes[i].value;
+    }
+
+    let targetId=document.getElementById(id);
+    console.log(id);
+    targetId.appendChild(newElement);
+
+    return newElement;
+  }
+}
+
+class course extends Component 
+{
+  constructor(name,author,date,ratings)
+  {
+    super();
+
     this.name=name;
     this.author=author;
     this.date=date;
     this.ratings=ratings;
+  }
+
+  render()
+  {
+    let newCourse=this.createAndAppendElement('li','',[],'courseList');
+
+
+    newCourse.innerHTML=`
+    <div style="text-align:center;border:3px solid blue">
+    <h1>${this.name}</h1>
+    <h2>${this.author}</h2>
+    <h3>${this.date}</h3>
+    <h4>${this.ratings}</h4>
+    ${(this.price)?this.price:""}
+    </div>
+    `
+  }
 }
 
 
-course.prototype.add=function() {
-  console.log("add"); 
-}
-
-course.prototype.render=function()
+class SpecialCourse extends course
 {
-  let newCourse=document.createElement('li');
-  newCourse.innerHTML=`
-  <div style="text-align:center;border:3px solid blue">
-  <h1>${this.name}</h1>
-  <h2>${this.author}</h2>
-  <h3>${this.date}</h3>
-  <h4>${this.ratings}</h4>
-  ${(this.price)?this.price:""}
-  </div>
-  `
-  return newCourse;
+  constructor(name,author,date,ratings,price)
+  {
+    super(name,author,date,ratings);
+    this.price=price;
+  }
 }
 
-
-
-function specialCourse(name,author,date,ratings,price) {
-
-  course.call(this,name,author,date,ratings);
-  this.price=price;
-}
-
-specialCourse.prototype=new course();
-specialCourse.prototype.constructor=specialCourse;
-
-
-
-const course1=new specialCourse("JS","Utkarsh","10/10/10",5,600);
-const course2=new course("JS","Utkarsh","10/10/10",5);
-console.log(course1);
-console.log(course2);
-
-
-function courseList(courses=[]) {
-
-  this.courses=[...courses]
-}
-
-courseList.prototype.addCourse=function(course)
+class CourseList extends Component
 {
+   courses=[];
+
+  constructor(courses)
+  {
+    super();
+    this.courses=[...courses]
+  }
+
+  addCourse(course)
+  {
     this.courses.push(course);
-}
+  }
 
-courseList.prototype.render= function()
-{
-  let courseListUI=document.createElement('ol');
+  removeCourse(courseId)
+  {
+    this.courses=this.courses.filter((course)=>
+    {
+      return course.id!==courseId;
+    });
+  }
 
-    console.log(this.courses);
+  render()
+  {
+    
+    let courseListUI=this.createAndAppendElement('ol','',[new attribute("id","courseList")],'app');
+    console.log(courseListUI);
 
     this.courses.forEach( (course) => {
-      let courseUI=course.render();
-      console.log(courseUI);
-      courseListUI.appendChild(courseUI);
+      course.render();
+
     });
 
-    return courseListUI;
+  }
 }
 
 
-function init()
+class Application
+{
+  static init()
   {
 
     const course1=new course("JS","Utkarsh","10/10/20",5);
     const course2=new course("React","Rahul","4/10/20",2);
-    const course3=new specialCourse("Nodejs","Rohit","3/10/20",4,500);
-    const courseListNew=new courseList([course1,course2,course3]);
-     const courseListUI=courseListNew.render();
-     console.log(courseListUI);
-     const app=document.querySelector("#app");
-     app.appendChild(courseListUI);
+    const course3=new SpecialCourse("Nodejs","Rohit","3/10/20",4,500);
+
+
+     const courseList=new CourseList([course1,course2,course3]);
+     courseList.render();
+    //  console.log(courseListUI);
+    //  const app=document.querySelector("#app");
+    //  app.appendChild(courseListUI);
   }
 
+}
 
-
-init();
+Application.init();
 
 
 
