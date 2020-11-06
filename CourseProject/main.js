7bf857f1e87e0add9a4d99457d579f8687f423ec
mohitverma6161 <utkarshmalik06@gmail.com>
2020-11-06 22:59:11 +0530

    bubble vs capturing

--- CourseProject/main.js
+++ CourseProject/main.js
@@ -1,104 +1,104 @@
-const addCourseButton=document.querySelector("#addCourseButton");
-const addMovieModal=document.querySelector("#course_modal");
-const backDrop=document.querySelector("#backdrop");
-const inputs=document.querySelectorAll("#modal_inputs input");
-const cancelCourseButton=document.querySelector("#modal_controls .btn-passive");
-const addCourseConfirmButton=document.querySelector("#modal_controls .btn-danger");
+// const addCourseButton=document.querySelector("#addCourseButton");
+// const addMovieModal=document.querySelector("#course_modal");
+// const backDrop=document.querySelector("#backdrop");
+// const inputs=document.querySelectorAll("#modal_inputs input");
+// const cancelCourseButton=document.querySelector("#modal_controls .btn-passive");
+// const addCourseConfirmButton=document.querySelector("#modal_controls .btn-danger");
 
 
 
 
-function clearInputs()
-{
-    inputs[0].value="";
-    inputs[1].value="";
-    inputs[2].value="";   
-}
+// function clearInputs()
+// {
+//     inputs[0].value="";
+//     inputs[1].value="";
+//     inputs[2].value="";   
+// }
 
-function onBackDropClick()
-{
-    addMovieModal.classList.toggle("visible");
-    backDrop.classList.toggle("visible");
-    clearInputs();
+// function onBackDropClick()
+// {
+//     addMovieModal.classList.toggle("visible");
+//     backDrop.classList.toggle("visible");
+//     clearInputs();
 
-}
-
-
-addCourseButton.addEventListener("click",()=>
-{
-    onBackDropClick()   
-})
+// }
 
 
-backDrop.addEventListener("click",()=>
-{
-    onBackDropClick()
-})
-
-cancelCourseButton.addEventListener("click",()=>
-{
-    onBackDropClick();
-})
+// addCourseButton.addEventListener("click",()=>
+// {
+//     onBackDropClick()   
+// })
 
 
-const allCourseList=[];
+// backDrop.addEventListener("click",()=>
+// {
+//     onBackDropClick()
+// })
+
+// cancelCourseButton.addEventListener("click",()=>
+// {
+//     onBackDropClick();
+// })
 
 
-function  renderNewCourse(newCourse) {
+// const allCourseList=[];
 
-    const courseList=document.querySelector("main ul");
-    const newLi=document.createElement("ol");
 
-    const {id,courseName,courseImage,courseRatings}=newCourse;
+// function  renderNewCourse(newCourse) {
+
+//     const courseList=document.querySelector("main ul");
+//     const newLi=document.createElement("ol");
+
+//     const {id,courseName,courseImage,courseRatings}=newCourse;
     
-    newLi.addEventListener("click",()=>
-    {
-        let indexValue=null;
+//     newLi.addEventListener("click",()=>
+//     {
+//         let indexValue=null;
 
-        for(let i=0;i<allCourseList.length;i++)
-        {
-            if(allCourseList[i].id===newCourse.id)
-            {
-                indexValue=i;
-                break;
-            }
-        }
+//         for(let i=0;i<allCourseList.length;i++)
+//         {
+//             if(allCourseList[i].id===newCourse.id)
+//             {
+//                 indexValue=i;
+//                 break;
+//             }
+//         }
 
-        console.log(allCourseList);
+//         console.log(allCourseList);
 
-        allCourseList.splice(indexValue,1);
+//         allCourseList.splice(indexValue,1);
 
-        courseList.children[indexValue].remove();
+//         courseList.children[indexValue].remove();
 
-    })
+//     })
 
-   newLi.innerHTML=`
-    <div class="course_item card" >
+//    newLi.innerHTML=`
+//     <div class="course_item card" >
 
-    <div>
-    <img src=${courseImage} />
-    </div>
+//     <div>
+//     <img src=${courseImage} />
+//     </div>
 
-    <div>
-    <h1> ${courseName} </h1>
-    <h3> ${courseRatings}/5
-    </div>
-    </div>
-    `
+//     <div>
+//     <h1> ${courseName} </h1>
+//     <h3> ${courseRatings}/5
+//     </div>
+//     </div>
+//     `
 
-    courseList.appendChild(newLi);
-}
+//     courseList.appendChild(newLi);
+// }
 
 
-addCourseConfirmButton.addEventListener("click",()=>
-{
-    const courseName=inputs[0].value;
-    const courseImage=inputs[1].value;
-    const courseRatings=inputs[2].value;
-    const newCourse={id:Math.random().toString(),courseName,courseImage,courseRatings};
-    allCourseList.push(newCourse);
-    onBackDropClick();
-    renderNewCourse(newCourse);
+// addCourseConfirmButton.addEventListener("click",()=>
+// {
+//     const courseName=inputs[0].value;
+//     const courseImage=inputs[1].value;
+//     const courseRatings=inputs[2].value;
+//     const newCourse={id:Math.random().toString(),courseName,courseImage,courseRatings};
+//     allCourseList.push(newCourse);
+//     onBackDropClick();
+//     renderNewCourse(newCourse);
 
-})
+// })
 
--- CourseProject/temp.js
+++ CourseProject/temp.js
@@ -1,134 +1,45 @@
 
-// class course
-// {
-//   constructor(name,author,date,ratings)
-//   {
-//     this.name=name;
-//     this.author=author;
-//     this.date=date;
-//     this.ratings=ratings;
-//   }
 
-//   render()
-//   {
-//     let newCourse=document.createElement('li');
-//     newCourse.innerHTML=`
-//     <div style="text-align:center;border:3px solid blue">
-//     <h1>${this.name}</h1>
-//     <h2>${this.author}</h2>
-//     <h3>${this.date}</h3>
-//     <h4>${this.ratings}</h4>
-//     ${(this.price)?this.price:""}
-//     </div>
-//     `
-//     return newCourse;
-//   }
 
+// const button=document.querySelector("#formSubmitButton");
+
+
+// const buttonHandler=(event)=>{
+
+//   event.preventDefault();
+
+//   const field=document.querySelector("#name");
+//   console.log(field.value);
+
+//   button.removeEventListener("click",buttonHandler);
 // }
 
-function course(name,author,date,ratings)
+// button.addEventListener("click",buttonHandler);
+
+
+const box1=document.querySelector("#box1");
+const box2=document.querySelector("#box2");
+const box3=document.querySelector("#box3");
+
+
+
+
+box1.addEventListener("click",(event)=>
 {
-  this.name=name;
-  this.author=author;
-  this.date=date;
-  this.ratings=ratings;
+  console.log("I am box 1");
+},false);
 
-}
-
-course.prototype.render=function()
+box2.addEventListener("click",(event)=>
 {
-  let newCourse=document.createElement('li');
-  newCourse.innerHTML=`
-  <div style="text-align:center;border:3px solid blue">
-  <h1>${this.name}</h1>
-  <h2>${this.author}</h2>
-  <h3>${this.date}</h3>
-  <h4>${this.ratings}</h4>
-  ${(this.price)?this.price:""}
-  </div>
-  `
-  return newCourse;
-}
-
-const c1=new course("JS");
-const c2=new course("React");
-
-console.log(c1);
-console.log(c2);
+  console.log("I am box 2");
+},true);
 
 
-function SpecialCourse(name,author,date,ratings,price)
+box3.addEventListener("click",(event)=>
 {
-  course.call(this,name,author,date,ratings);
-  this.price=price;
-}
-
-
-SpecialCourse.prototype=new course();
-SpecialCourse.prototype.constructor=SpecialCourse;
-
-const sc=new SpecialCourse();
-console.log(sc.__proto__.__proto__.__proto__);
-
-
-// class CourseList
-// {
-//    courses=[];
-
-//   constructor(courses)
-//   {
-//     this.courses=[...courses]
-//   }
-
-//   addCourse(course)
-//   {
-//     this.courses.push(course);
-//   }
-
-//   removeCourse(courseId)
-//   {
-//     this.courses=this.courses.filter((course)=>
-//     {
-//       return course.id!==courseId;
-//     });
-//   }
-
-// render()
-// {
-
-//   let courseListUI=document.createElement('ol');
-
-//   console.log(this.courses);
-
-//   this.courses.forEach( (course) => {
-//     let courseUI=course.render();
-//     console.log(courseUI);
-//     courseListUI.appendChild(courseUI);
-//   });
-
-//   return courseListUI;
-// }
-// }
+  console.log("I am box 3");
+},false);
 
 
 
-// class Application
-// {
-//    static init()
-//   {
 
-//     const course1=new course("JS","Utkarsh","10/10/20",5);
-//     const course2=new course("React","Rahul","4/10/20",2);
-//     const course3=new SpecialCourse("Nodejs","Rohit","3/10/20",4,500);
-
-
-//      const courseList=new CourseList([course1,course2,course3]);
-//      const courseListUI=courseList.render();
-//      console.log(courseListUI);
-//      const app=document.querySelector("#app");
-//      app.appendChild(courseListUI);
-//   }
-
-// }
-
-// Application.init();
