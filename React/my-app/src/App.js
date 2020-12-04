23ea0dfd404a00177baa608eda9922559262ab96
mohitverma6161 <utkarshmalik06@gmail.com>
2020-12-04 21:32:47 +0530

    state props project

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,19 +1,53 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
+import {Spinner} from 'react-bootstrap';
 import Component1 from './component1';
 import UserComp from './userComponent';
 
+/*
+
+Lifecycle methods :
+
+*/
 
 class App extends Component {
 
   constructor(props)
   {
+    console.log("In constructor");
+
     super(props);
 
     this.state={
-      users:[{name:"Utkarsh",age:34},{name:"Rahuk",age:34},{name:"rohan",age:34},{name:"Rohini",age:34}],
+      loading:true,
+      users:[],
       language:"Hindi"
     }
   }
+
+  componentDidMount()
+  {
+    //api call
+
+    fetch('https://dummyapi.io/data/api/user',{
+      headers:{
+        "app-id":"5fca5a3236a334a6a8f40cc7"
+      }
+    })
+    .then(response => response.json())
+    .then(users=>{
+      console.log(users);
+      this.setState(
+        {
+          loading:false,
+          users:users.data
+        }
+      )
+    })
+
+  }
+
 
   onToggleLanguage()
   {
@@ -24,26 +58,40 @@ class App extends Component {
 
   render()
   {
+
+    console.log("render");
+
   return (
     <div  className="App">
       <header className="App-header">
-      <button onClick={ ()=>this.onToggleLanguage() } > Toggle Language </button>
+      {
+      //  <button onClick={ ()=>this.onToggleLanguage() } > Toggle Language </button>
+      }
       <Component1/>
       <h1>
       {
-        (this.state.language==="English")?"How are you ?":"Aap kaise ho ?"   
+        // (this.state.language==="English")?"How are you ?":"Aap kaise ho ?"   
       }
-      
+
+
       {
-        this.state.users.map((user)=>
-        {
-          return(
-           <UserComp  userDetails={user}  />
-          )
-        })
+
+         (this.state.loading===true)?
+         (<Spinner style={{margin:"50px"}} size="lg" animation="border" role="status">
+         <span className="sr-only">Loading...</span>
+       </Spinner>):
+       (this.state.users.map((user)=>
+          {
+            return(
+             <UserComp  userDetails={user}  />
+            )
+          })
+         )
 
       }
+    
 
+      
       </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- React/my-app/src/userComponent.js
+++ React/my-app/src/userComponent.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {Card,Button} from 'react-bootstrap';
 
 function userComponent(props) {
 
-    const {userDetails}=props;
+
+    const {firstName,lastName,picture,email}=props.userDetails;
 
     return (
-        <div>
-        Name:{userDetails.name}
-        Age:{userDetails.age}
-        </div>
+        <Card style={{ width:"18rem",color:"black",display:"inline-block",margin:"10px",border:"3px solid black" }}>
+        <Card.Img variant="top" src={picture} />
+        <Card.Body>
+          <Card.Title>           {firstName + " " + lastName}
+          </Card.Title>
+          <Card.Text>
+          {email}
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>   
     );
 }
 
